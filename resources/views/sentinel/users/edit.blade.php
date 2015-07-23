169d3e844c33efaf320b1f58c8ab8b3e0596ed07
wayhi <JW@shiwenbodeMacBook-Pro.local>
2015-07-23 12:52:29 +0800

    Initalization

---
+++ resources/views/sentinel/users/edit.blade.php
@@ -0,0 +1,149 @@
+@extends(config('sentinel.layout'))
+
+{{-- Web site Title --}}
+@section('title')
+@parent
+Edit Profile
+@stop
+
+{{-- Content --}}
+@section('content')
+
+<?php
+    // Pull the custom fields from config
+    $isProfileUpdate = ($user->email == Sentry::getUser()->email);
+    $customFields = config('sentinel.additional_user_fields');
+
+    // Determine the form post route
+    if ($isProfileUpdate) {
+        $profileFormAction = route('sentinel.profile.update');
+        $passwordFormAction = route('sentinel.profile.password');
+    } else {
+        $profileFormAction =  route('sentinel.users.update', $user->hash);
+        $passwordFormAction = route('sentinel.password.change', $user->hash);
+    }
+?>
+
+<div class="row">
+    <h1>
+        Edit
+        @if ($isProfileUpdate)
+            Your
+        @else
+            {{ $user->email }}'s
+        @endif
+        Account
+    </h1>
+</div>
+
+<?php $customFields = config('sentinel.additional_user_fields'); ?>
+
+@if (! empty($customFields))
+
+<div class="row">
+    <div class="small-6 large-centered columns">
+        <form method="POST" action="{{ $profileFormAction }}" accept-charset="UTF-8" class="form-horizontal" role="form">
+            <input name="_method" value="PUT" type="hidden">
+            <input name="_token" value="{{ csrf_token() }}" type="hidden">
+
+            <h4>Profile</h4>
+            
+            @foreach(config('sentinel.additional_user_fields') as $field => $rules)
+            <div class="row">
+                <div class="small-3 columns">
+                    <label for="right-label" class="right inline">{{ ucwords(str_replace('_',' ',$field)) }}</label>
+                </div>
+                <div class="small-9 columns {{ ($errors->has($field)) ? 'has-error' : '' }}">
+                    <input name="{{ $field }}" type="text" value="{{ Input::old($field) ? Input::old($field) : $user->$field }}">
+                    {{ ($errors->has($field) ? $errors->first($field, '<small class="error">:message</small>') : '') }}
+                </div>
+            </div>
+            @endforeach
+
+            <div class="row">
+                <div class="small-9 small-offset-3 columns">
+                    <input class="button" value="Submit Changes" type="submit">
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+@endif
+
+
+@if (Sentry::getUser()->hasAccess('admin') && ($user->hash != Sentry::getUser()->hash))
+<form method="POST" action="{{ route('sentinel.users.memberships', $user->hash) }}" accept-charset="UTF-8" class="form-horizontal" role="form">
+    <div class="row">
+        <div class="small-6 large-centered columns">
+            <h4>Group Memberships</h4>
+            
+            <div class="row">
+                <div class="small-9 small-offset-3 columns">
+                    @foreach($groups as $group)
+                        <label class="checkbox-inline">
+                            <input type="checkbox" name="groups[{{ $group->name }}]" value="1" {{ ($user->inGroup($group) ? 'checked' : '') }}> {{ $group->name }}
+                        </label>
+                    @endforeach
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="small-9 small-offset-3 columns">
+                    <input name="_token" value="{{ csrf_token() }}" type="hidden">
+                    <input class="button" value="Update Memberships" type="submit">
+                </div>
+            </div>
+
+        </div>
+    </div>
+</form>
+@endif
+
+
+<form method="POST" action="{{ $passwordFormAction }}" accept-charset="UTF-8" class="form-inline" role="form">
+    <div class="row">
+        <div class="small-6 large-centered columns">
+            <h4>Change Password</h4>    
+    
+            <div class="row">
+                <div class="small-3 columns">
+                    <label for="right-label" class="right inline">Current</label>
+                </div>
+                <div class="small-9 columns">
+                    <input placeholder="Old Password" name="oldPassword" value="" id="oldPassword" type="password">
+                    {{ ($errors->has('oldPassword') ?  $errors->first('oldPassword', '<small class="error">:message</small>') : '') }}
+                </div>
+            </div>
+
+             <div class="row">
+                <div class="small-3 columns">
+                    <label for="right-label" class="right inline">New</label>
+                </div>
+                <div class="small-9 columns">
+                    <input class="form-control" placeholder="New Password" name="newPassword" value="" id="newPassword" type="password">
+                    {{ ($errors->has('newPassword') ?  $errors->first('newPassword', '<small class="error">:message</small>') : '') }}
+                </div>
+            </div>
+
+             <div class="row">
+                <div class="small-3 columns">
+                    <label for="right-label" class="right inline">Confirm</label>
+                </div>
+                <div class="small-9 columns">
+                    <input class="form-control" placeholder="Confirm New Password" name="newPassword_confirmation" value="" id="newPassword_confirmation" type="password">
+                    {{ ($errors->has('newPassword_confirmation') ?  $errors->first('newPassword_confirmation', '<small class="error">:message</small>') : '') }}
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="small-9 small-offset-3 columns">
+                    <input name="_token" value="{{ csrf_token() }}" type="hidden">
+                    <input class="button" value="Change Password" type="submit">
+                </div>
+            </div>
+
+        </div>
+    </div>
+</form>
+
+@stop
---
+++ resources/views/sentinel/users/index.blade.php
@@ -0,0 +1,52 @@
+@extends(config('sentinel.layout'))
+
+{{-- Web site Title --}}
+@section('title')
+@parent
+Home
+@stop
+
+{{-- Content --}}
+@section('content')
+<div class="row">
+	<div class="large-6 columns">
+		<h1>Current Users</h1>
+	</div>
+	<div class="large-6 columns right">
+		<button class="button" onClick="location.href='{{ route('sentinel.users.create') }}'">Create User</button>
+	</div>
+</div>
+
+
+<div class="row">
+	<table class="full-width">
+		<thead>
+			<th>User</th>
+			<th>Status</th>
+			<th>Options</th>
+		</thead>
+		<tbody>
+			@foreach ($users as $user)
+				<tr>
+					<td><a href="{{ action('\\Sentinel\Controllers\UserController@show', array($user->hash)) }}">{{ $user->email }}</a></td>
+					<td>{{ $user->status }} </td>
+					<td>
+						<button class="button small" type="button" onClick="location.href='{{ action('\\Sentinel\Controllers\UserController@edit', array($user->hash)) }}'">Edit</button>
+						@if ($user->status != 'Suspended')
+							<button class="button small" type="button" onClick="location.href='{{ action('\\Sentinel\Controllers\UserController@suspend', array($user->hash)) }}'">Suspend</button>
+						@else
+							<button class="button small" type="button" onClick="location.href='{{ action('\\Sentinel\Controllers\UserController@unsuspend', array($user->hash)) }}'">Un-Suspend</button>
+						@endif
+						@if ($user->status != 'Banned')
+							<button class="button small" type="button" onClick="location.href='{{ action('\\Sentinel\Controllers\UserController@ban', array($user->hash)) }}'">Ban</button>
+						@else
+							<button class="button small" type="button" onClick="location.href='{{ action('\\Sentinel\Controllers\UserController@unban', array($user->hash)) }}'">Un-Ban</button>
+						@endif
+						<button class="button small alert action_confirm" href="{{ action('\\Sentinel\Controllers\UserController@destroy', array($user->hash)) }}" data-token="{{ Session::getToken() }}" data-method="delete">Delete</button>
+					</td>
+				</tr>
+			@endforeach
+		</tbody>
+	</table>
+</div>
+@stop
